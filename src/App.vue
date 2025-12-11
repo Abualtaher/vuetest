74c45ed471aa557f30b35bb32d343859c6a09771
wisam-altaher <wisam.al-taher@postnord.com>
2025-12-11 11:39:28 +0100

    just trying things with tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterView, RouterLink } from "vue-router";
+import My from "./components/My.vue";
 </script>
 <template>
+  <My />
   <div class="p-10 text-center">
     <h1 class="text-4xl font-bold text-blue-600">Tailwind is working! 🎉</h1>
   </div>

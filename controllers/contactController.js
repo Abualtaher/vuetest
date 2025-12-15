a74638c996654decaf0bedb8647e8920eee5b8b1
wisam-altaher <wisam.al-taher@postnord.com>
2025-12-15 21:23:14 +0100

    contacController add

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,3 +1,31 @@
 //@desc Get all contacts
 //@route Get /api/contacts
 //@access public
+
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+};
+
+const creatContact = (req, res) => {
+  res.status(201).json({ message: "Creat new contact" });
+};
+
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+};
+
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+};
+
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+};
+
+module.exports = {
+  getContacts,
+  creatContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
